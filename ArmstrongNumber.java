11829f5f1321adcf0c27629345f8c89b174753df
Mobisha2008 <mobisha490@gmail.com>
2026-08-20 19:01:49 +0530

    ArmstrongNumber

---
+++ ArmstrongNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int temp = n;
+        int sum = 0;
+        int digits = String.valueOf(n).length();
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, digits);
+            temp /= 10;
+        }
+
+        if (sum == n)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
+    }
+}
